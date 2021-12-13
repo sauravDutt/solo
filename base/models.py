36468b1eaeb8094cad8b979e69140d448fe228cc
sauravDutt <sauravd069@gmail.com>
2021-12-13 11:34:13 +0530

    Just added some more stuff

--- base/models.py
+++ base/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.base import Model
 
 # Create your models here.
+class Topics(models.Model):
+    name = models.CharField(max_length=2000)
+
+    def __str__(self):
+        return self.name
+
+class Post(models.Model):
+    host = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    topic = models.ForeignKey(Topics, on_delete=models.SET_NULL, null= True)
+    title = models.CharField(max_length=200)
+    discription = models.TextField(null=True, blank=True)
+    updated = models.DateTimeField(auto_now= True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-updated', '-created']
+
+    def __str__(self):
+        return self.name
+
+class Message(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    body = models.TextField()
+    updated = models.DateTimeField(auto_now = True)
+    created = models.DateTimeField(auto_now_add = True)
+
+    def __str__(self):
+        return self.body[0:50]
