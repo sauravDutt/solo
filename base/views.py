081138ffd213a360ce70894f1de88a0d06c96e3b
sauravDutt <sauravd069@gmail.com>
2021-12-14 14:16:16 +0530

    Just added some more stuff

--- base/views.py
+++ base/views.py
@@ -1,8 +1,23 @@
 from django.http.response import HttpResponse
 from django.shortcuts import render
+from django.db.models import Q
+from .models import Message, Post, Topic
 
 # Create your views here.
 
-def home(requenst):
-    return render(requenst, 'base/home.html')
+def home(request):
+    q = request.GET.get('q') if request.GET.get('q') != None else ''
+
+    posts = Post.objects.filter(
+        Q(topic__name__icontains= q) |
+        Q(title__icontains=q) |
+        Q(discription__icontains=q)
+        )
+
+    topics = Topic.objects.all()
+    post_count = posts.count()
+    post_messages = Message.objects.all()
+
+    context = {'posts': posts, 'topics': topics, 'post_count': post_count, 'post_messages': post_messages}
+    return render(request, 'base/home.html', context)
 
