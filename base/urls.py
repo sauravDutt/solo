671566af2d01bc828efb172a82639116c1fcfc4a
sauravDutt <sauravd069@gmail.com>
2021-12-15 13:08:00 +0530

    Just added some more stuff

--- base/urls.py
+++ base/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('login/', views.loginPage, name='login'),
     path('logout/', views.logoutUser, name='logout'),
     path('register/', views.registerPage, name='register'),
-    
+
     path('', views.home, name='home'),
     path('post/<str:pk>/', views.post, name='post'),
 ]
